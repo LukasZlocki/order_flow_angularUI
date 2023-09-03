770d97dff450ceeddb6045338a32cbd1bc7dd97e
lzlocki <lzlocki@poczta.fm>
2023-09-03 11:14:50 +0200

    Add routings to orders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrdersComponent } from './components/orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OrdersComponent
+  },
+  {
+    path: 'orders',
+    component: OrdersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
